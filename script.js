fb7e0edc21946d734ea9c52be95a2af9841ad717
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-03-09 16:35:08 +0300

    Something added and some code refactored

--- script.js
+++ script.js
@@ -1,87 +1,21 @@
 
+// const words=["Hotels","Restaurants","Shops","Salons","Apartments","Travels","Business","Fitness"];
+// const element=document.getElementById('word');
+// setInterval(() => {
+//   let word_index=Math.floor(Math.random() * words.length);
+//   element.innerHTML=`${words[word_index]}</p>`
+// }, 1000);
 
+const words=["Hotels", "Restauransts","Shops","Salons","Apartments","Travels","Business","Fitness"];
 
-// const spanList = [...document.querySelector('.cd-words-wrapper')];
-// const selectors = document.querySelectorAll('.cd-words-wrapper > span');
-//     const list = [...selectors].map(span => parseInt(span.innerText.replace(/"/g,"")));
-//     console.log(list)
-//console.log(document.getElementsByClassName('.is-hidden').innerHTML=list[1].innerHTML);
-
-// console.log(document.querySelectorAll('span').innerHTML)=list.length;
-
-
-
-// function isHidden(list) {
-//   for (var item in list) {
-//     document.querySelectorAll('.cd-words-wrapper').innerHTML=list[item];
-    
-//       console.log(list[item]);
-//     }
-//   return (list.offsetParent === null)
-// }
-
-
-// function isVisible(domElement) {
-//   return new Promise(resolve => {
-//     const o = new IntersectionObserver(([entry]) => {
-//       resolve(entry.intersectionRatio === 1);
-//       o.disconnect();
-//     });
-//     o.observe(domElement);
-//   });
-// }
-
-// const visible = await isVisible(document.querySelector('.cd-words-wrapper'));
-// console.log(visible);
-
-//const list=["Hotels","Restaurants","Shops","Salons","Apartments","Travels"];
-
-// function toggle_visibility(){
-//   const list=["Hotels","Restaurants","Shops","Salons","Apartments","Travels"];
-//   document.getElementsByClassName('is-hidden');
-//   for (var i=0; i<list.length; ++i){
-//      var s = list[i].style;
-//      s.display = s.display==='none' ? 'block' : 'none';
-//   };
-// }
-// console.log(toggle_visibility('is-hidden'));
-
-
-// function toggleText(){
-//   var elms = document.getElementsByClassName("is-hidden");
-
-//   Array.from(elms).forEach((x) => {
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-
-
-//   })
-// }
-
-// console.log(toggleText('is-hidden'));
-
-// var list = document.getElementsByClassName("is-hidden");
-// for (var i = 0; i < list.length; i++) {
-//  list[i].onclick = function() {
-//     document.getElementById('is-hidden').classList.toggle("myactive");
-//   }
-// }
-
-// const list=["Hotels","Restaurants","Shops","Salons","Apartments","Travels"];
-// var x=document.getElementById('demo').innerHTML=list;
-// for (let i = 0; i < list.length; i++) {
-//   list[1].style.visibility="hidden"
-// }
-
-
-const words=["Hotels","Restaurants","Shops","Salons","Apartments","Travels","Business","Fitness"];
 const element=document.getElementById('word');
+
+let index=0;
+
 setInterval(() => {
-  let word_index=Math.floor(Math.random() * words.length);
-  element.innerHTML=`${words[word_index]}</p>`
-}, 1000);
-
-
+  element.innerHTML=`${words[index]}`
+  index++;
+  if(index == words.length){
+    index = 0;
+  }
+},1000);
